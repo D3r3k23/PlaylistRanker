04a3fb88620f37f2c8241e388b9cd13f68107f09
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-05-05 17:41:59 -0700

    Setup

---
+++ src/PlaylistRanker.py
@@ -0,0 +1,31 @@
+
+from dataclasses import dataclass
+from enum import Enum
+
+playlistURL = ""
+
+class DataType(Enum):
+    Track  = 0
+    Artist = 1
+    Album  = 2
+
+    def __str__(self):
+        if self is DataType.Track:
+            return 'Album'
+        if self is DataType.Artist:
+            return 'Artist'
+        if self is DataType.Album:
+            return 'Album'
+
+@dataclass(eq=False)
+class Song:
+    """Contains information about a song"""
+    title:  str
+    artist: str
+    album:  str
+
+if __name__ == "__main__":
+
+    songs = [] # Load from playlistURL
+
+    scrobbles = [] # Load from LastFmApi
